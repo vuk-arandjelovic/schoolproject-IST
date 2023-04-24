d8d163a481c0e24de05d964be6841e92b2f16414
Vuk Arandjelovic <arandjelovicvuk@gmail.com>
2023-04-24 17:17:57 +0200

    Final touches, minor bug fixes

--- src/components/MoviePopular.js
+++ src/components/MoviePopular.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Typography, Grid, Paper, Container, Stack, Rating } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 
 // const movies = [{naziv:"Spiderman", ocena:"8.8",reziser:"Mario Vreco",trajanje:"210",tride:"da"},
 // {naziv:"John Wick", ocena:"9.9",reziser:"Milutin Pavkovic",trajanje:"390",tride:"da"},
@@ -22,9 +24,11 @@ function MoviePopular(){
                     <Stack alignItems="center" py={3}>
                     <Typography gutterBottom variant="h4">{value?.naziv}</Typography>
                     <Typography gutterBottom><Rating readOnly value={value?.ocena || 0} /></Typography>
-                    <Typography gutterBottom>Reziser: {value?.reziser}</Typography>
+                    <Typography variant="h6" fontWeight={500} gutterBottom>Reziser: {value?.reziser}</Typography>
                     <Typography gutterBottom>Trajanje: {value?.trajanje}min</Typography>
-                    <Typography gutterBottom>3D: {value?.tride === true? "Jeste" : "Nije"}</Typography>
+                    <Typography gutterBottom textAlign={"center"}>
+                        3D: {value?.tride === true? (<CheckIcon color="success" sx={{verticalAlign: "bottom"}}/>):(<CloseIcon color="error" sx={{verticalAlign: "bottom"}}/>)}
+                    </Typography>
                     </Stack>
                 </Paper>
             </Grid>
